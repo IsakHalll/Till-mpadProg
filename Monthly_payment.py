f9d6fe0d42c74bdeacc61d053c166f01dc964aab
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2024-08-20 10:41:07 +0200

    Monthly payment

---
+++ Monthly_payment.py
@@ -0,0 +1,14 @@
+def calculate_monthly_payment(principal,annual_rate,years):
+     monthly_rate = annual_rate/100/12
+     months = years*12
+     payment = (principal * monthly_rate*(1+monthly_rate)**months) / ((1 + monthly_rate)**months - 1)
+     return payment
+
+
+principal = float(input("Lånebelopp: "))
+annual_rate = float(input("Årsränta (%): "))
+years = int(input("Löptid (år): "))
+payment = calculate_monthly_payment(principal, annual_rate, years)
+print(f"Din månatliga betalning är: {payment:.2f} kr")
+
+    
